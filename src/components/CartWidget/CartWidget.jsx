c445fd8a5ca9c766e21ee580aa474272ee3d2bf0
alan.r <reale.ag@gmail.com>
2022-09-07 08:56:49 -0300

    itemcount y listcontainer de component a funcion

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
-import { Component } from "react";
 
-class CartWidget extends Component {
-    render() {
-        return (
-            <i class="fa-solid fa-cart-shopping"></i>
-        )
-    }
+const CartWidget = () => {
+    return(
+        <i class="fa-solid fa-cart-shopping"></i>
+    )
 }
-
 
 export default CartWidget;
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
-import { Component } from "react";
 
-class ItemListContainer extends Component {
-    render() {
+const ItemListContainer = () => {
         return (
             <div className='d-flex justify-content-center text-decoration-underline font-monospace m-3'>
             <h1>{this.props.proyecto}</h1>
             </div>
         )
-    }
 }
 
 
