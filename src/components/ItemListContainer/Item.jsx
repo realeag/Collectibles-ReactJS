faf687e19c66d89b3984cfc4115a1cb2d69707f3
alan.r <reale.ag@gmail.com>
2022-09-07 22:12:31 -0300

    fix rutas imgs GRACIAS DIEGO

--- src/components/ItemListContainer/Item.jsx
+++ src/components/ItemListContainer/Item.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
+import './Item.css';
 
 const Item = ({ figuritas }) => {
     return (
-        < div className="card tarjeta" >
-            <img src={figuritas.imagen} className="card-img-top" alt={figuritas.descripcion}/>
-                <div className="card-body">
-                    <h5 className="card-title">{figuritas.nombre}</h5>
-                    <p className="card-text">$ {figuritas.precio}</p>
-                    <a className="btn btn-primary">AÑADIR</a>
-                </div>
+        < div className="card m-1" >
+            <img src={figuritas.imagen} className="card-img-top imagen" alt={figuritas.descripcion} />
+            <div className="card-body">
+                <h5 className="card-title">{figuritas.nombre}</h5>
+                <p className="card-text">$ {figuritas.precio}</p>
+                <a className="btn btn-primary">AÑADIR</a>
             </div>
+        </div>
     );
 }
 
