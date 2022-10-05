13e7311b67204d963554ba2e2b36e6cabfa3c6d5
alan.r <reale.ag@gmail.com>
2022-10-05 10:55:02 -0300

    fix error msg etiqueta <a>

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,21 +1,20 @@
 import React from 'react';
 import { useContext } from 'react';
-import { Link } from 'react-router-dom';
 import { CartContext } from '../../context/CartContext';
 
 const CartWidget = () => {
     const { totalItems } = useContext(CartContext);
     const totalUnits = totalItems();
 
+    const renderCount = () => {
+        return totalUnits ? <span>{totalUnits}</span> : false;
+    }
+
     return (
-        (totalUnits === 0)
-            ?
-            <Link to='/cart' className="fa-solid fa-cart-shopping"></Link>
-            :
-            <div>
-                <Link to='/cart' className="fa-solid fa-cart-shopping"></Link>
-                <span> {totalUnits}</span>
-            </div>
+        <div>
+            <span className="fa-solid fa-cart-shopping"></span>
+            {''} { renderCount() }
+        </div>
     )
 };
 
